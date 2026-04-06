944a5ea9bc1cbd51440b29093e31e924587e2d40
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-04-06 21:13:17 +0530

    customise mini statusline

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -11,50 +11,78 @@ return {
             -- when this plugin loads, i want to not show the mode
             vim.o.showmode = false
             local statusline = require("mini.statusline")
+
+            local function get_branch()
+                local branch = vim.b.gitsigns_head
+                if branch and branch ~= "" then
+                    -- return "⎇ " .. branch
+                    return " " .. branch
+                end
+                return ""
+            end
+
+            statusline.setup({
+                content = {
+                    active = function()
+                        local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+
+                        -- git branch
+                        -- local branch = vim.fn.system("git branch --show-current 2>/dev/null"):gsub("%s+", "")
+                        local git = get_branch()
+
+                        -- filename + modified flag
+                        local filename = vim.fn.expand("%:t") -- just the filename, no path
+                        local modified = vim.bo.modified and " ●" or ""
+                        local file = filename ~= "" and (filename .. modified) or "[No Name]"
+
+                        -- diagnostics
+                        local diag = statusline.section_diagnostics({ trunc_width = 75 })
+
+                        -- right side
+                        local location = "%l:%c"
+                        local filetype = vim.bo.filetype
+
+                        return statusline.combine_groups({
+                            { hl = mode_hl,                    strings = { mode } },
+                            { hl = "MiniStatuslineBranch",     strings = { git } },
+                            { hl = "MiniStatuslineFilename",   strings = { file } },
+                            { hl = "MiniStatuslineDiagnostic", strings = { diag } },
+                            "%=", -- right align everything after this
+                            { hl = "MiniStatuslineFileinfo", strings = { filetype } },
+                            { hl = mode_hl,                  strings = { location } },
+                        })
+                    end,
+                },
+            })
+
             local colors = {
-                blue = "#65D1FF",
-                green = "#3EFFDC",
-                violet = "#FF61EF",
-                yellow = "#FFDA7B",
-                red = "#FF4A4A",
-                fg = "#c3ccdc",
-                bg = "#112638",
+                blue        = "#65D1FF",
+                green       = "#3EFFDC",
+                violet      = "#FF61EF",
+                yellow      = "#FFDA7B",
+                red         = "#FF4A4A",
+                fg          = "#c3ccdc",
+                bg          = "#112638",
                 inactive_bg = "#2c3043",
             }
 
-            local function hi(group, fg, bg, bold)
-                vim.api.nvim_set_hl(0, group, {
-                    fg = fg,
-                    bg = bg,
-                    bold = bold or false,
-                })
-            end
+            local hl = vim.api.nvim_set_hl
+            hl(0, "MiniStatuslineModeNormal", { fg = colors.bg, bg = colors.blue, bold = true })
+            hl(0, "MiniStatuslineModeInsert", { fg = colors.bg, bg = colors.green, bold = true })
+            hl(0, "MiniStatuslineModeVisual", { fg = colors.bg, bg = colors.violet, bold = true })
+            hl(0, "MiniStatuslineModeCommand", { fg = colors.bg, bg = colors.yellow, bold = true })
+            hl(0, "MiniStatuslineModeReplace", { fg = colors.bg, bg = colors.red, bold = true })
+            hl(0, "MiniStatuslineFilename", { fg = colors.fg, bg = colors.bg })
+            hl(0, "MiniStatuslineBranch", { fg = colors.fg, bg = colors.bg })
+            hl(0, "MiniStatuslineDiagnostic", { fg = colors.fg, bg = colors.bg })
+            hl(0, "MiniStatuslineFileinfo", { fg = colors.fg, bg = colors.inactive_bg })
+            hl(0, "MiniStatuslineInactive", { fg = colors.fg, bg = colors.inactive_bg })
 
-            -- Normal mode
-            hi("MiniStatuslineModeNormal", colors.bg, colors.blue, true)
-
-            -- Insert mode
-            hi("MiniStatuslineModeInsert", colors.bg, colors.green, true)
-
-            -- Visual mode
-            hi("MiniStatuslineModeVisual", colors.bg, colors.violet, true)
-
-            -- Command mode
-            hi("MiniStatuslineModeCommand", colors.bg, colors.yellow, true)
-
-            -- Replace mode
-            hi("MiniStatuslineModeReplace", colors.bg, colors.red, true)
-
-            -- Inactive
-            hi("MiniStatuslineInactive", colors.fg, colors.inactive_bg)
-
-            -- Default sections
-            hi("MiniStatuslineDevinfo", colors.fg, colors.bg)
-            hi("MiniStatuslineFilename", colors.fg, colors.bg)
-            hi("MiniStatuslineFileinfo", colors.fg, colors.bg)
-
-            statusline.setup({
-                use_icons = true,
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "GitSignsUpdate",
+                callback = function()
+                    vim.cmd("redrawstatus")
+                end,
             })
 
             vim.api.nvim_create_autocmd({ "ModeChanged", "CursorMoved", "CursorMovedI" }, {
@@ -62,6 +90,6 @@ return {
                     vim.cmd("redrawstatus")
                 end,
             })
-        end,
+        end
     },
 }
