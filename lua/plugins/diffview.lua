68c4353ac644a295d24e9cd220f6839204123633
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-04-17 13:33:38 +0530

    add diffview keybindings

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,5 +1,38 @@
+local function toggle_diffview(cmd)
+    if next(require("diffview.lib").views) == nil then
+        vim.cmd(cmd)
+    else
+        vim.cmd("DiffviewClose")
+    end
+end
+
 return {
     {
         "sindrets/diffview.nvim",
+        command = "DiffviewOpen",
+        -- cond = is_git_root,
+        keys = {
+            {
+                "<leader>gd",
+                function()
+                    toggle_diffview("DiffviewOpen")
+                end,
+                desc = "Diff Index",
+            },
+            {
+                "<leader>gD",
+                function()
+                    toggle_diffview("DiffviewOpen main..HEAD")
+                end,
+                desc = "Diff master",
+            },
+            {
+                "<leader>gf",
+                function()
+                    toggle_diffview("DiffviewFileHistory %")
+                end,
+                desc = "Open diffs for current File",
+            },
+        }
     },
 }
