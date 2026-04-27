a678db411464d593b24d309839206336eff7678a
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-04-27 09:27:51 +0000

    feat: remove cursorline + codediff plugin addition

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -14,15 +14,15 @@ opt.autoindent = true
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true  -- if you include mixed case in your search, assumes you want
+opt.smartcase = true -- if you include mixed case in your search, assumes you want
 
-opt.cursorline = true
+opt.cursorline = false
 
 -- turn on termguicolors for nightfly colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
