5dbc284906a20e3111dd5db614245c0a33d8ef35
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-05-14 06:54:20 +0530

    set min fold lines

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -41,6 +41,7 @@ return {
                         vim.opt_local.foldmethod = "expr"
                         vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
                     end
+                    vim.opt_local.foldminlines = 100
 
                     vim.schedule(function()
                         -- Only run normal if we're not in terminal mode
