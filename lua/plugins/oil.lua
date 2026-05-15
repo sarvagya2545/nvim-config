2bac09793f62de40b94844d6c207459f65317b45
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-05-15 21:04:03 +0530

    feat: add oil nvim keymaps

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,3 +1,21 @@
 return {
-    { "https://github.com/stevearc/oil.nvim", opts = {} }
+    {
+        "https://github.com/stevearc/oil.nvim",
+        enabled = true,
+        opts = {},
+        config = function()
+            require("oil").setup({
+                keymaps = {
+                    ["<C-h>"] = false,
+                    ["<C-l>"] = false,
+                },
+                view_options = {
+                    show_hidden = true,
+                },
+            })
+
+            vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+            vim.keymap.set("n", "<leader>-", require("oil").toggle_float, { desc = "Open Oil in floating window" })
+        end,
+    },
 }
