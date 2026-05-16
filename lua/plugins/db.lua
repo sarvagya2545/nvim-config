b216f7cb2fe44c78a4fed20462719111e746e1d1
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-05-17 00:57:05 +0530

    fix: stop execute on save in db

--- lua/plugins/db.lua
+++ lua/plugins/db.lua
@@ -1,5 +1,15 @@
 return {
-    "tpope/vim-dadbod",
-    "kristijanhusak/vim-dadbod-ui",
-    "kristijanhusak/vim-dadbod-completion",
+    { "tpope/vim-dadbod" },
+    {
+        "kristijanhusak/vim-dadbod-ui",
+        dependencies = {
+            "tpope/vim-dadbod",
+            "kristijanhusak/vim-dadbod-completion",
+        },
+        init = function()
+            vim.g.db_ui_execute_on_save = 0
+        end,
+        cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection" },
+    },
+    { "kristijanhusak/vim-dadbod-completion" },
 }
