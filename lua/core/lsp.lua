b3a8c78d72c9477f8cc76b404ab0ea17cbaadb87
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-05-17 18:31:33 +0530

    feat: add debugger setup for golang

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,4 +1,4 @@
-vim.lsp.enable({ "lua_ls", "clangd", "ruff", "ty", "ts_ls", "groovyls" })
+vim.lsp.enable({ "lua_ls", "clangd", "ruff", "ty", "ts_ls", "groovyls", "gopls" })
 
 vim.filetype.add({
     pattern = {
---
+++ lua/plugins/debugger.lua
@@ -0,0 +1,56 @@
+return {
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "leoluz/nvim-dap-go",
+            "rcarriga/nvim-dap-ui",
+            "theHamsta/nvim-dap-virtual-text",
+            "nvim-neotest/nvim-nio",
+            "williamboman/mason.nvim",
+        },
+        config = function()
+            local dap = require("dap")
+            local ui = require("dapui")
+
+            require("dapui").setup()
+            require("dap-go").setup()
+            require("nvim-dap-virtual-text").setup()
+
+            -- KEYMAPS
+            vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
+            vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
+            vim.keymap.set("n", "<leader>dr", dap.repl.open)
+
+            -- Eval var under cursor
+            vim.keymap.set("n", "<space>?", function()
+                require("dapui").eval(nil, { enter = true })
+            end)
+
+            vim.keymap.set("n", "<F1>", dap.continue)
+            vim.keymap.set("n", "<F2>", dap.step_into)
+            vim.keymap.set("n", "<F3>", dap.step_over)
+            vim.keymap.set("n", "<F4>", dap.step_out)
+            vim.keymap.set("n", "<F5>", dap.step_back)
+            vim.keymap.set("n", "<F12>", dap.restart)
+
+            -- UI handlers: open when launching
+            dap.listeners.before.attach.dapui_config = function()
+                ui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                ui.open()
+            end
+
+            -- close after everything is done
+            dap.listeners.before.event_terminated.dapui_config = function()
+                ui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                ui.close()
+            end
+
+            -- open and close dap ui keymaps
+            vim.keymap.set("n", "<leader><F1>", ui.toggle, { desc = "Toggle DAP UI" })
+        end,
+    },
+}
