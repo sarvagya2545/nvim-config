0e6e57bbd8164a3573ddf0d0d3f38a416f0a1b04
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-05-25 22:46:06 +0530

    feat: add markdown preview and markdown render plugins

--- lua/plugins/md.lua
+++ lua/plugins/md.lua
@@ -1,6 +1,7 @@
 return {
     {
         "delphinus/md-render.nvim",
+        enabled = false,
         version = "*",
         dependencies = {
             { "nvim-tree/nvim-web-devicons", version = "*" }, -- optional: file type icons in code blocks
@@ -11,5 +12,23 @@ return {
             { "<leader>mt", "<Plug>(md-render-preview-tab)", desc = "Markdown preview in tab (toggle)" },
             { "<leader>md", "<Plug>(md-render-demo)", desc = "Markdown render demo" },
         },
+    },
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        -- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && npm install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
     },
 }
