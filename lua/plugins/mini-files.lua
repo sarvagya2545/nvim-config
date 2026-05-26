81347212c4c5e85b5c3106d234574ce8a7146435
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-05-26 05:14:43 +0000

    feat(mini.files): toggle keybindings

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -1,3 +1,10 @@
+local function toggleExplorer(...)
+    local MiniFiles = require("mini.files")
+    if not MiniFiles.close() then
+        MiniFiles.open(...)
+    end
+end
+
 return {
     "nvim-mini/mini.files",
     enabled = true,
@@ -17,20 +24,21 @@ return {
         {
             "<leader>ef",
             function()
-                require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+                toggleExplorer(vim.api.nvim_buf_get_name(0), true)
             end,
             desc = "Open mini.files (Directory of Current File)",
         },
         {
             "<leader>ee",
             function()
-                require("mini.files").open(vim.uv.cwd(), true)
+                toggleExplorer(vim.uv.cwd(), true)
             end,
             desc = "Open mini.files (cwd)",
         },
     },
     config = function(_, opts)
-        require("mini.files").setup(opts)
+        local MiniFiles = require("mini.files")
+        MiniFiles.setup(opts)
 
         local show_dotfiles = true
         local filter_show = function(fs_entry)
@@ -43,21 +51,21 @@ return {
         local toggle_dotfiles = function()
             show_dotfiles = not show_dotfiles
             local new_filter = show_dotfiles and filter_show or filter_hide
-            require("mini.files").refresh({ content = { filter = new_filter } })
+            MiniFiles.refresh({ content = { filter = new_filter } })
         end
 
         local map_split = function(buf_id, lhs, direction, close_on_file)
             local rhs = function()
                 local new_target_window
-                local cur_target_window = require("mini.files").get_explorer_state().target_window
+                local cur_target_window = MiniFiles.get_explorer_state().target_window
                 if cur_target_window ~= nil then
                     vim.api.nvim_win_call(cur_target_window, function()
                         vim.cmd("belowright " .. direction .. " split")
                         new_target_window = vim.api.nvim_get_current_win()
                     end)
 
-                    require("mini.files").set_target_window(new_target_window)
-                    require("mini.files").go_in({ close_on_file = close_on_file })
+                    MiniFiles.set_target_window(new_target_window)
+                    MiniFiles.go_in({ close_on_file = close_on_file })
                 end
             end
 
@@ -102,11 +110,11 @@ return {
             end,
         })
 
-        vim.api.nvim_create_autocmd("User", {
-            pattern = "MiniFilesActionRename",
-            callback = function(event)
-                Snacks.rename.on_rename_file(event.data.from, event.data.to)
-            end,
-        })
+        -- vim.api.nvim_create_autocmd("User", {
+        --     pattern = "MiniFilesActionRename",
+        --     callback = function(event)
+        --         Snacks.rename.on_rename_file(event.data.from, event.data.to)
+        --     end,
+        -- })
     end,
 }
