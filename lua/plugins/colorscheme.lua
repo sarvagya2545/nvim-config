6ea04924852869f09c198f6df5689eec3e4bdf4a
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-06-13 06:41:09 +0530

    feat(colorscheme): transparent background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -52,4 +52,74 @@ return {
             vim.cmd("colorscheme catppuccin")
         end,
     },
+    {
+        "xiyaowong/transparent.nvim",
+        config = function()
+            -- Optional, you don't have to run setup.
+            require("transparent").setup({
+                -- table: default groups
+                groups = {
+                    "Normal",
+                    "NormalNC",
+                    "Comment",
+                    "Constant",
+                    "Special",
+                    "Identifier",
+                    "Statement",
+                    "PreProc",
+                    "Type",
+                    "Underlined",
+                    "Todo",
+                    "String",
+                    "Function",
+                    "Conditional",
+                    "Repeat",
+                    "Operator",
+                    "Structure",
+                    "LineNr",
+                    "NonText",
+                    "SignColumn",
+                    "CursorLine",
+                    "CursorLineNr",
+                    "StatusLine",
+                    "StatusLineNC",
+                    "EndOfBuffer",
+                },
+                -- table: additional groups that should be cleared
+                extra_groups = {
+                    "NormalFloat",
+                    "NvimTreeNormal",
+
+                    -- Telescope
+                    "TelescopeNormal",
+                    "TelescopeBorder",
+                    "TelescopePromptNormal",
+                    "TelescopePromptBorder",
+                    "TelescopePromptTitle",
+                    "TelescopePreviewNormal",
+                    "TelescopePreviewBorder",
+                    "TelescopePreviewTitle",
+                    "TelescopeResultsNormal",
+                    "TelescopeResultsBorder",
+                    "TelescopeResultsTitle",
+
+                    -- Mini.files
+                    "MiniFilesBorder",
+                    "MiniFilesBorderModified",
+                    "MiniFilesBackground",
+                    "MiniFilesCursorLine",
+                    "MiniFilesDirectory",
+                    "MiniFilesFile",
+                    "MiniFilesNormal",
+                    "MiniFilesTitle",
+                    "MiniFilesTitleFocused",
+                },
+                -- table: groups you don't want to clear
+                exclude_groups = {},
+                -- function: code to be executed after highlight groups are cleared
+                -- Also the user event "TransparentClear" will be triggered
+                on_clear = function() end,
+            })
+        end,
+    },
 }
