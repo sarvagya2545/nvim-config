10a6687d11fe91b3f9302d0b6f62df60cef01bbd
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-06-20 12:51:20 +0530

    feat: add bullets plugin

--- lua/plugins/md.lua
+++ lua/plugins/md.lua
@@ -1,18 +1,5 @@
 return {
-    {
-        "delphinus/md-render.nvim",
-        enabled = false,
-        version = "*",
-        dependencies = {
-            { "nvim-tree/nvim-web-devicons", version = "*" }, -- optional: file type icons in code blocks
-            { "delphinus/budoux.lua", version = "*" }, -- optional: CJK phrase-level line breaking
-        },
-        keys = {
-            { "<leader>mp", "<Plug>(md-render-preview)", desc = "Markdown preview (toggle)" },
-            { "<leader>mt", "<Plug>(md-render-preview-tab)", desc = "Markdown preview in tab (toggle)" },
-            { "<leader>md", "<Plug>(md-render-demo)", desc = "Markdown render demo" },
-        },
-    },
+    { "https://github.com/bullets-vim/bullets.vim" }, -- for auto bullet points and lists
     {
         "MeanderingProgrammer/render-markdown.nvim",
         -- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
