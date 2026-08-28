38734d3f6b9a5d25de9d9239bdbd69b46cd50351
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-08-28 17:04:55 +0530

    feat(latex): create artifacts in build folder

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -4,6 +4,18 @@ return {
         init = function()
             vim.g.vimtex_view_method = "skim"
             vim.g.vimtex_quickfix_open_on_warning = false
+            vim.g.vimtex_compiler_latexmk = {
+                out_dir = "build",
+                callback = 1,
+                continuous = 1,
+                executable = "latexmk",
+                options = {
+                    "-verbose",
+                    "-file-line-error",
+                    "-synctex=1",
+                    "-interaction=nonstopmode",
+                },
+            }
         end,
     },
 }
